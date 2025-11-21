d09a39f622b2e0021c66621088c1d22afb337b5f
SenaYora <matthieu.veiga@gmail.com>
2025-11-21 02:10:19 +0100

    fix: redirections

--- functions/api/[[catchall]].js
+++ functions/api/[[catchall]].js
@@ -2,20 +2,34 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
   const targetUrl = url.toString().replace(url.origin, 'https://opfr-back.vercel.app');
 
-  const newRequest = new Request(targetUrl, {
+  const proxyRequest = new Request(targetUrl, {
     method: context.request.method,
     headers: context.request.headers,
     body: context.request.body,
     redirect: 'manual'
   });
 
-  const response = await fetch(newRequest);
+  const response = await fetch(proxyRequest);
 
-  const newResponseHeaders = new Headers(response.headers);
+  const newHeaders = new Headers(response.headers);
+
+  newHeaders.delete('content-encoding');
+  newHeaders.delete('content-length');
+  newHeaders.delete('transfer-encoding');
+
+  if (response.status >= 300 && response.status < 400) {
+    const location = newHeaders.get('location');
+    if (location) {
+      return new Response(null, {
+        status: response.status,
+        headers: newHeaders
+      });
+    }
+  }
 
   return new Response(response.body, {
     status: response.status,
     statusText: response.statusText,
-    headers: newResponseHeaders
+    headers: newHeaders
   });
 }
