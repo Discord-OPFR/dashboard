be931a23d38f23b45b43ad8c59a11d0602b6aa73
SenaYora <matthieu.veiga@gmail.com>
2025-11-21 02:01:58 +0100

    feat: update functions to manual redirections

--- functions/api/[[catchall]].js
+++ functions/api/[[catchall]].js
@@ -1,15 +1,21 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  const newUrl = url
-    .toString()
-    .replace(url.origin, 'https://opfr-back.vercel.app');
+  const targetUrl = url.toString().replace(url.origin, 'https://opfr-back.vercel.app');
 
-  const newRequest = new Request(newUrl, {
+  const newRequest = new Request(targetUrl, {
     method: context.request.method,
     headers: context.request.headers,
     body: context.request.body,
-    redirect: 'follow',
+    redirect: 'manual'
   });
 
-  return fetch(newRequest);
+  const response = await fetch(newRequest);
+
+  const newResponseHeaders = new Headers(response.headers);
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newResponseHeaders
+  });
 }
